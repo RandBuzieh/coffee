8a634d2ed00ff491bec51438cc59bb1da3fa4966
RandBuzieh <107172513+RandBuzieh@users.noreply.github.com>
2022-06-15 10:32:51 +0300

    Update java.js

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,3 +1,24 @@
+const cohorts = "interestCohort";
+const documentProto = Document.prototype;
+const flocSupported = cohorts in documentProto;
+
+if (!flocSupported) {
+  return;
+}
+
+const descriptor = Object.getOwnPropertyDescriptor(documentProto, cohorts);
+const writable = descriptor && descriptor.writable;
+if (writable) {
+  const proxy = new Proxy(documentProto[cohorts], { apply: () => Promise.reject() });
+  const config = {
+    writable: false,
+    value: proxy,
+    configurable: false,
+    enumerable: false,
+  };
+  Object.defineProperty(documentProto, cohorts, config);
+}
+
 let slideIndex = 0;
 showSlides();
 function showSlides() {
